6bbacf5f17873210707e80b6312087bd06790bb9
David Burch <daburch@clemson.edu>
2021-01-28 21:06:34 -0500

    draw pieces

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Piece from './Piece';
 
-const Square = (props) => {
-    return (<div className={ "square square-" + props.color } />);
+class Square extends Component {
+    state = {
+        color: "white",
+        piece: "none"
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { color: props.color, piece: props.piece };
+    }
+
+    buildPiece() {
+        if (this.props.piece === "none") {
+            return ""
+        }
+        return <Piece color={ this.props.piece } />
+    }
+
+    render() {
+        return (
+            <div className={ "square square-" + this.state.color }> 
+                { this.buildPiece() }
+            </div>
+        );
+    }
 }
  
 export default Square;
